ac007b84eaabb03a3ad72789b9c062ddcfe2791b
hz9527 <981379316@qq.com>
2017-11-13 23:51:07 +0800

    :beers: nothing

--- template/build/dev-serve.js
+++ template/build/dev-serve.js
@@ -11,7 +11,7 @@ var getHtml = require('./utils/transHtml.js')
 // { // entryObj
 //   index_html: {
 //     $tem: code,
-//     srcs: ['_js_index_index_js'],
+//     srcs: {_js_index_index_js: state}, // 0 not bundle 1 pending 2 finish
 //     wsList: {id: ws}
 //   }
 // }
@@ -26,8 +26,29 @@ var getHtml = require('./utils/transHtml.js')
 var entryList = tools.getHtmlPathList()
 var localIp = tools.localIp
 
+// 打包完成通知enrtyObj，entryObj确认是否需要重新加载
+// 需要重载时ws会给所有客户端发送一个页面标识，如果页面与标识一致就发送id给服务端，服务端收到id后将相应的id的ws删除并告知客户端，客户端再进行刷新
+// websocket收到连接时生成一个id通知客户端
+
 // 遍历入口，解析占位符，打包占位符对应的文件，并watch入口列表，watch占位符对应的包
 var entryObj = {}, fileDep = {}
+entryObj.on = function (type, data) { // deps pathName
+  var state
+  if (type === 'bundleStart') {
+    state = 1
+  } else if (type === 'bundleEnd') {
+    state = 2
+  }
+  data.deps.forEach(htmlName => {
+    this._setState(htmlName, data.pathName, state)
+  })
+}
+entryObj._setState = function (htmlName, pathName, state) {
+  this[htmlName].srcs[pathName] = state
+  if (Object.values(this[htmlName].srcs).every(s => s === 2)) {
+    // update pages
+  }
+}
 Promise.all(entryList.map(item => { // item ==> path
   return getHtml(item, true)
 }))
@@ -55,7 +76,7 @@ Promise.all(entryList.map(item => { // item ==> path
   })
 
 function getWatcher (filePath, pathName) {
-  var watcher = rollup.watch(getOptions('./js/index/index.js', true))
+  var watcher = rollup.watch(getOptions(filePath, true))
   watcher.on('event', event => {
     if (event.code === 'BUNDLE_END') {
 
